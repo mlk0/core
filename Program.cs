7a569293234e2403d17564c353c84f9aaf712bc9
mlk0 <acme@gmx.com>
2017-07-01 10:41:37 -0400

    initial commit for some dotnet core on ubuntu, should look like like any other console app in c# but this one was developet on ubuntu 16.04

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myThing = System.Environment.MachineName;
+            var message = String.Format("Hi mirko at {0}", myThing);
+            Console.WriteLine(message);
+            
+
+
+        }
+    }
+}
